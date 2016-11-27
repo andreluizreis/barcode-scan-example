ebb7a2b86bb83edf609f724f3dfc7e15433b78ae
racla <andreluizreis@gmail.com>
2016-11-26 23:21:23 -0500

    Camera auto focus fixed.

--- app/src/main/java/andreluizreis/barcodedetect/CameraActivity.java
+++ app/src/main/java/andreluizreis/barcodedetect/CameraActivity.java
@@ -3,6 +3,9 @@ package andreluizreis.barcodedetect;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +19,9 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Field;
 
 /**
  * Camera Activity Class
@@ -23,6 +29,21 @@ import java.io.IOException;
 public class CameraActivity extends AppCompatActivity {
 
     SurfaceView cameraPreview;
+
+    /**
+     * Custom annotation to allow only valid focus modes.
+     */
+    @StringDef({
+            Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE,
+            Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO,
+            Camera.Parameters.FOCUS_MODE_AUTO,
+            Camera.Parameters.FOCUS_MODE_EDOF,
+            Camera.Parameters.FOCUS_MODE_FIXED,
+            Camera.Parameters.FOCUS_MODE_INFINITY,
+            Camera.Parameters.FOCUS_MODE_MACRO
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    private @interface FocusMode {}
 
     /**
      * On Create
@@ -58,6 +79,7 @@ public class CameraActivity extends AppCompatActivity {
 
                 try {
                     cameraSource.start(cameraPreview.getHolder());
+                    cameraFocus(cameraSource, Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -91,5 +113,38 @@ public class CameraActivity extends AppCompatActivity {
                 }
             }
         });
+    } // createCameraSource
+
+    public static boolean cameraFocus(@NonNull CameraSource cameraSource, @FocusMode @NonNull String focusMode) {
+        Field[] declaredFields = CameraSource.class.getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            if (field.getType() == Camera.class) {
+                field.setAccessible(true);
+                try {
+                    Camera camera = (Camera) field.get(cameraSource);
+                    if (camera != null) {
+                        Camera.Parameters params = camera.getParameters();
+
+                        if (!params.getSupportedFocusModes().contains(focusMode)) {
+                            return false;
+                        }
+
+                        params.setFocusMode(focusMode);
+                        camera.setParameters(params);
+                        return true;
+                    }
+
+                    return false;
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
+        }
+
+        return false;
     }
+
 }
